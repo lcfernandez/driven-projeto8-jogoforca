2a346846872c2624518d416d0fb63c0980179927
lcfernandez <lcfernandez@cos.ufrj.br>
2022-09-21 01:30:40 -0300

    Import de CSS e imagem do estado inicial

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import ReactDOM from 'react-dom';
 
+import "./assets/css/reset.css";
+import "./assets/css/style.css";
+
+import forca0 from "./assets/images/forca0.png";
+
 function App() {
     return (
-        <></>
+        <img src={forca0} alt="Forca no estado inicial" />
     );
 }
 
